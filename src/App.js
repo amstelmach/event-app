a1b055fb9f971518221ebc6e287d9131c9d01806
Alicja <alicjaastelmach@gmail.com>
2022-11-09 10:21:01 +0100

    cache API response in localStorage

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     getApiData();
   }, [])
 
   const getApiData = async() => {
-    const api = await fetch(`https://gist.githubusercontent.com/codeholic/23e37417db35be1fa89060b360abb164/raw/deb4e8dd41c3df43fffb7e7f7770a38cd8cf5d40/event_venues.json`);
-    const response = await api.json();
-    setData(response);
+    const dataCheck = localStorage.getItem('data');
+    if(dataCheck){
+      setData(JSON.parse(dataCheck));
+    } else {
+      const api = await fetch(`https://gist.githubusercontent.com/codeholic/23e37417db35be1fa89060b360abb164/raw/deb4e8dd41c3df43fffb7e7f7770a38cd8cf5d40/event_venues.json`);
+      const response = await api.json();
+      localStorage.setItem('data', JSON.stringify(response));
+      setData(response);
+    }
   }
 
   return (
