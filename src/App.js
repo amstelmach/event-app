1b6dec6f556182f0bff31cc6b112974a4c729599
Alicja <alicjaastelmach@gmail.com>
2022-11-09 11:08:44 +0100

    add filter to display venues by ids

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState([]);
+  const [filtered, setFiltered] = useState([]);
 
   useEffect(() => {
     getApiData();
@@ -20,13 +21,38 @@ function App() {
     }
   }
 
+  const filterData = e => {
+    const { value, checked } = e.target;
+    if(!filtered.includes(value) && checked) {
+      setFiltered([...filtered, value])
+    } else {
+      setFiltered(filtered.filter(item => item !== value))
+    }
+  }
+
+  const filteredPlaces = filtered.length > 0 ? data.filter(place => filtered.includes(place.title)) : data;
+
   return (
-    <div>
+    <div className="main-container">
+      <h2>Filter by id:</h2>
+      <div className="checkbox-container">
+        {
+          data.map(item => {
+            return(
+              <div className="single-checkbox" key={item.id}>
+                <input type="checkbox" value={item.title} onChange={filterData}/>
+                <label>{item.id}</label>
+              </div>
+            )
+          })
+        }
+      </div>
+      <h2>Places:</h2>
       {
-        data.map(item => {
+        filteredPlaces.map(place => {
           return(
-            <div key={item.id}>
-              <h4>{item.title}</h4>
+            <div key={place.id}>
+              <h4>{place.title}</h4>
             </div>
           )
         })
